68fc4b70e84262c691ec312a530c5af7cb192da3
Oğulcan Eğri <155257820+ogulcanegri@users.noreply.github.com>
2024-02-14 14:31:21 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('nav');
+    const navToggle = document.querySelector('.nav-toggle');
+});
