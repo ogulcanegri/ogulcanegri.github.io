28c259998e3217163c9058c45963b76c40dbf631
Oğulcan Eğri <155257820+ogulcanegri@users.noreply.github.com>
2024-02-17 17:56:27 +0200

    Delete scripts directory

--- scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('nav');
-    const navToggle = document.querySelector('.nav-toggle');
-});
